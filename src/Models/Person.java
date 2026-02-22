6b5aaa583ce18faffa8c4ca770d9b42c9102b45d
akimana2001 <dativeakimana@gmail.com>
2026-02-22 20:22:03 +0200

    Implemented abstract Person base class with encapsulation

--- src/Models/Person.java
+++ src/Models/Person.java
@@ -1,5 +1,41 @@
 package Models;
 
+public abstract class Person {
 
-public class Person {
+    private String id;
+    private String name;
+    private String email;
+
+    public Person(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Email: " + email;
+    }
 }

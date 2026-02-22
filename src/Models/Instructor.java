b88205937302e51a9278bc406bf4f753a87382a8
akimana2001 <dativeakimana@gmail.com>
2026-02-22 20:57:26 +0200

    Implemented Instructor class

--- src/Models/Instructor.java
+++ src/Models/Instructor.java
@@ -1,4 +1,27 @@
 package Models;
 
-public class Instructor {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Instructor extends Person {
+
+    private String department;
+    private List<Course> coursesTeaching = new ArrayList<>();
+
+    public Instructor(String id, String name, String email, String department) {
+        super(id, name, email);
+        this.department = department;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public List<Course> getCoursesTeaching() {
+        return coursesTeaching;
+    }
+
+    public void addCourse(Course course) {
+        coursesTeaching.add(course);
+    }
 }

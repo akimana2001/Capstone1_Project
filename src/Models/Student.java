de025e7b4c19103d251879fcfbe25f78308d8300
akimana2001 <dativeakimana@gmail.com>
2026-02-22 20:34:41 +0200

    Added abstract Student class with course-grade mapping

--- src/Models/Student.java
+++ src/Models/Student.java
@@ -1,5 +1,55 @@
 package Models;
 
+import java.util.HashMap;
+import java.util.Map;
 
-public class Student {
+public abstract class Student extends Person {
+
+    private String studentID;
+    private double GPA;
+    private String department;
+
+    private Map<Course, Double> enrolledCourses = new HashMap<>();
+
+    public Student(String id, String name, String email,
+                   String studentID, double GPA, String department) {
+        super(id, name, email);
+        this.studentID = studentID;
+        this.GPA = GPA;
+        this.department = department;
+    }
+
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public Map<Course, Double> getEnrolledCourses() {
+        return enrolledCourses;
+    }
+
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+
+    public void enrollCourse(Course course, double grade) {
+        enrolledCourses.put(course, grade);
+    }
+
+
+    public abstract double calculateTuition();
 }

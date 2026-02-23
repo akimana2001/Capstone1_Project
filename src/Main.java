702a7bc052560a3de8ce9c4ef9e4a0507f55ab0b
akimana2001 <dativeakimana@gmail.com>
2026-02-23 15:18:22 +0200

    The finalizing lab2 Exception Handling and Busines Logic

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import Models.*;
+import Service.UniversityManager;
+import Exceptions.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        UniversityManager manager = new UniversityManager();
+
+
+        Student s1 = new GraduateStudent(
+                "1", "Alice", "alice@email.com",
+                "S001", 3.8, "Computer Science");
+
+        Student s2 = new GraduateStudent(
+                "2", "Bob", "bob@email.com",
+                "S002", 3.5, "Computer Science");
+
+        // Assigning courses
+        Course c1 = new Course("CS101", "Java Programming", 3, 1);
+        Course c2 = new Course("CS102", "Python", 4, 2);
+
+        // Registering students and course
+        manager.registerStudent(s1);
+        manager.registerStudent(s2);
+        manager.createCourse(c1);
+
+        // Trying enrollment
+        try {
+            manager.enrollStudentInCourse(s1, c1);
+            manager.enrollStudentInCourse(s2, c1);
+        } catch (CourseFullException | StudentAlreadyEnrolledException e) {
+            System.out.println("Enrollment failed: " + e.getMessage());
         }
+
+        // Assigning the grades
+        manager.assignGrade(s1, c1, 4.0);
+
+        // Calculating average GPA
+        double avg = manager.calculateAverageGPAByDepartment("Computer Science");
+        System.out.println("Average GPA (Computer Science): " + avg);
+
+        // Finding top student
+        manager.findTopStudent()
+                .ifPresent(student ->
+                        System.out.println("Top Student: " + student.getName())
+                );
     }
 }
